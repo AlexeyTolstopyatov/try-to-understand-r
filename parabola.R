5993ab740c004764511c2bfd36425477afdf04e8
Billbo Backends <45635607+AlexeyTolstopyatov@users.noreply.github.com>
2024-10-03 11:07:52 -0400

    Add files via upload

---
+++ parabola.R
@@ -0,0 +1,11 @@
+# plot arguments
+# 'l' -- Line
+# 'b' -- Line and Dots (both)
+# 'n' -- no
+
+x <- 1:100
+y <- x * x
+
+data.frame(x, y) # build table of X and Y values
+
+plot(x, y, type = "l") # build 'парабола' chart
